1d5c71b73a5e561aa7db37ae7fd1b8348c638ee1
JayShamis <jays63@nycstudents.net>
2025-12-03 10:19:21 -0500

    2025d2p1

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,46 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        ArrayList<String> lines = getFileData("src/data");
-
-        int partOneAnswer = 0;
-        int partTwoAnswer = 0;
-        partOneAnswer=getPartOneNumber(lines);
-        partTwoAnswer=getPartTwoNumber(lines);
+//        ArrayList<String> lines = getFileData("src/data");
+        String[] ranges = getFileData("src/data").get(0).split(",");
+        int partOneAnswer;
+        int partTwoAnswer;
+        partOneAnswer=getPartOneNumber(ranges);
+        partTwoAnswer=getPartTwoNumber(ranges);
         System.out.println("Part one answer: " + partOneAnswer);
         System.out.println("Part two answer: " + partTwoAnswer);
     }
 
-    public static int getPartOneNumber(ArrayList<String> lines) {
-        int pos=50;
-        int count=0;
-        for (int i = 0; i < lines.size(); i++) {
-            if (lines.get(i).charAt(0) == 'L') {
-                pos -= Integer.parseInt(lines.get(i).substring(1));
-                while (pos < 0) {
-                    pos += 100;
+    public static int getPartOneNumber(String[] ranges) {
+        int invSum=0;
+        System.out.println(Arrays.toString(ranges)); //DEBUG PRINT
+        for (int i = 0; i < ranges.length; i++) {
+            String[] things=ranges[i].split("-");
+            int minNum=Integer.parseInt(things[0]);
+            int maxNum=Integer.parseInt(things[1]);
+            for (int j = minNum; j <= maxNum; j++) {
+                String thing=""+j;
+                if (thing.length()%2==0){
+                    String first=thing.substring(0, thing.length()/2);
+                    String second=thing.substring(thing.length()/2);
+                    if (first.equals(second)) {
+                        invSum += j;
+//                        System.out.println(j); DEBUG PRINT
+                    }
                 }
-            }
-            else if (lines.get(i).charAt(0) == 'R') {
-                pos += Integer.parseInt(lines.get(i).substring(1));
-                while (pos >= 100) {
-                    pos -= 100;
-                }
-            }
-            if (pos == 0) {
-                count++;
             }
         }
-        return count;
+        return invSum;
     }
 
-    public static int getPartTwoNumber(ArrayList<String> lines) {
-        int pos=50;
-        int count=0;
-        for (int i = 0; i < lines.size(); i++) {
-            if (lines.get(i).charAt(0) == 'L') {
-                for (int x = Integer.parseInt(lines.get(i).substring(1)); x > 0 ; x--) {
-                    pos--;
-                    if (pos%100==0) {
-                        pos=0;
-                        count++;
-                    }
-                }
-                if (pos < 0)
-                    pos+=100;
-            }
-            else if (lines.get(i).charAt(0) == 'R') {
-                for (int x = Integer.parseInt(lines.get(i).substring(1)); x > 0; x--){
-                    pos++;
-                    if (pos%100==0) {
-                        pos=0;
-                        count++;
-                    }
-                    if (pos > 100) {
-                        pos-=100;
-                    }
-                }
-            }
-            System.out.println("Position: "+pos+" Count: "+count+" Line: "+(1+i));
-        }
-        return count;
+    public static int getPartTwoNumber(String[] ranges) {
+        return 0;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
