d6ea3bed07133194b0d992306ff5665fbc0893f0
JayShamis <jays63@nycstudents.net>
2025-12-04 10:09:39 -0500

    Day2, part2, done

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,28 @@ public class Main {
     public static void main(String[] args) {
 //        ArrayList<String> lines = getFileData("src/data");
         String[] ranges = getFileData("src/data").get(0).split(",");
-        long partOneAnswer;
-        int partTwoAnswer;
+        Long partOneAnswer;
+        Long partTwoAnswer;
         partOneAnswer=getPartOneNumber(ranges);
         partTwoAnswer=getPartTwoNumber(ranges);
         System.out.println("Part one answer: " + partOneAnswer);
         System.out.println("Part two answer: " + partTwoAnswer);
     }
 
-    public static long getPartOneNumber(String[] ranges) {
-        long invSum=0;
+    public static Long getPartOneNumber(String[] ranges) {
+        Long invSum= 0L;
         System.out.println(Arrays.toString(ranges)); //DEBUG PRINT
         for (int i = 0; i < ranges.length; i++) {
             String[] things=ranges[i].split("-");
-            System.out.println(Arrays.toString(things));
-            long minNum=Long.parseLong(things[0]);
-            long maxNum=Long.parseLong(things[1]);
-            for (long j = minNum; j <= maxNum; j++) {
+            Long minNum=Long.parseLong(things[0]);
+            Long maxNum=Long.parseLong(things[1]);
+            for (Long j = minNum; j <= maxNum; j++) {
                 String thing=""+j;
                 if (thing.length()%2==0){
                     String first=thing.substring(0, thing.length()/2);
                     String second=thing.substring(thing.length()/2);
                     if (first.equals(second)) {
                         invSum += j;
-//                        System.out.println(j); DEBUG PRINT
                     }
                 }
             }
@@ -40,8 +38,34 @@ public class Main {
         return invSum;
     }
 
-    public static int getPartTwoNumber(String[] ranges) {
-        return 0;
+    public static Long getPartTwoNumber(String[] ranges) {
+        Long invSum=0L;
+        System.out.println(Arrays.toString(ranges)); //DEBUG PRINT
+        for (int i = 0; i < ranges.length; i++) {
+            String[] things=ranges[i].split("-");
+            Long minNum=Long.parseLong(things[0]);
+            Long maxNum=Long.parseLong(things[1]);
+            for (Long j = minNum; j <= maxNum; j++) {
+                String thing = Long.toString(j);
+                for (int x=1; x<thing.length(); x++){
+                    String pattern=thing.substring(0, x);
+                    String temp=thing;
+                    int a=pattern.length();
+                    while (a<=temp.length()){
+                        if (!temp.substring(0, a).equals(pattern)){
+                            break;
+                        }
+                        temp=temp.substring(a);
+                    }
+                    if (temp.isEmpty()){
+                        invSum+=Long.parseLong(thing);
+                        System.out.println(thing);
+                        break;
+                    }
+                }
+            }
+        }
+        return invSum;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
