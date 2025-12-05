1eb235f6a9a7f46bb63b3a2633f8ae2f791a6971
JayShamis <jays63@nycstudents.net>
2025-12-05 10:20:25 -0500

    Starting day 3

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,53 @@ public class Main {
     public static void main(String[] args) {
 //        ArrayList<String> lines = getFileData("src/data");
 //        String[] ranges = getFileData("src/data").get(0).split(",");
+        ArrayList<String> data=getFileData("src/data");
         int partOneAnswer;
         int partTwoAnswer;
-        partOneAnswer=getPartOneNumber();
-        partTwoAnswer=getPartTwoNumber();
+        partOneAnswer=getPartOneNumber(data);
+        partTwoAnswer=getPartTwoNumber(data);
         System.out.println("Part one answer: " + partOneAnswer);
         System.out.println("Part two answer: " + partTwoAnswer);
     }
 
-    public static int getPartOneNumber(String[] ranges) {
-        return 0;
+    public static int getPartOneNumber(ArrayList<String> input) {
+        int out=0;
+        String newRes="";
+        boolean caseB;
+        for (int lines = 0; lines < input.size(); lines++) {
+            int thing=0;
+            caseB=false;
+            for (int i = 9; i >0; i--) {
+                for (int j = 0; j < input.get(lines).length(); j++) {
+                    if (Integer.parseInt(input.get(lines).substring(j, j+1))==i){
+                        if (j!=input.get(lines).length()-1){
+                            newRes=input.get(lines).substring(j+1);
+                            thing+=i*10;
+                            i=0;
+                        } else {newRes=input.get(lines).substring(0, j); thing+=i; i=0; caseB=true;
+                            System.out.println(i);}
+                    }
+                }
+            }
+            for (int i = 9; i >0; i--) {
+                for (int j = 0; j < newRes.length(); j++) {
+                    if (Integer.parseInt(input.get(lines).substring(j, j+1))==i){
+                        if (caseB){
+                            thing+=10*i;
+                        } else {
+                            thing += i;
+                        }
+                        i=0;
+                    }
+                }
+            }
+            System.out.println(thing);
+            out+=thing;
+        }
+        return out;
     }
 
-    public static int getPartTwoNumber(String[] ranges) {
+    public static int getPartTwoNumber(ArrayList<String> input) {
         return 0;
     }
 
