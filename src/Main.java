90a5a6cdcb0168bbb665e55bed2be3e2d1f82642
JayShamis <jays63@nycstudents.net>
2025-12-08 10:15:24 -0500

    Day 3 part 2 is going to be annoying

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 //        String[] ranges = getFileData("src/data").get(0).split(",");
         ArrayList<String> data=getFileData("src/data");
         int partOneAnswer;
-        int partTwoAnswer;
+        long partTwoAnswer;
         partOneAnswer=getPartOneNumber(data);
         partTwoAnswer=getPartTwoNumber(data);
         System.out.println("Part one answer: " + partOneAnswer);
@@ -32,14 +32,13 @@ public class Main {
                             newRes=input.get(lines).substring(j+1);
                             thing+=i*10;
                             i=0;
-                        } else {newRes=input.get(lines).substring(0, j); thing+=i; i=0; caseB=true;
-                            System.out.println(i);}
+                        } else {newRes=input.get(lines).substring(0, input.get(lines).length()-1); thing+=i; i=0; caseB=true;}
                     }
                 }
             }
             for (int i = 9; i >0; i--) {
                 for (int j = 0; j < newRes.length(); j++) {
-                    if (Integer.parseInt(input.get(lines).substring(j, j+1))==i){
+                    if (Integer.parseInt(newRes.substring(j, j+1))==i){
                         if (caseB){
                             thing+=10*i;
                         } else {
@@ -55,8 +54,36 @@ public class Main {
         return out;
     }
 
-    public static int getPartTwoNumber(ArrayList<String> input) {
-        return 0;
+    public static long getPartTwoNumber(ArrayList<String> input) {
+        long out=0L;
+        for (int i = 0; i < input.size(); i++) {
+            int digitsRemaining=12;
+            Long increment=0L;
+            int lastDigitCount=0;
+            while (digitsRemaining>0){
+                String newRes=input.get(i);
+                for (int x = 9; x>0; x--) {
+                    for (int j = 0; j < newRes.length(); j++) {
+                        if (Integer.parseInt(newRes.substring(j, j+1))==x){
+                            if (j!=newRes.length()-1){
+                                newRes=input.get(i).substring(j+1);
+                                x=0;
+                                j--;
+                            } else if (Integer.parseInt(newRes.substring(newRes.length()-1))==x){newRes=newRes.substring(0, newRes.length()-1);
+                                increment+= x*(long)Math.pow(10, lastDigitCount);
+                                lastDigitCount++;
+                                x=0;
+                                j--;
+                            }
+                        }
+                    }
+                    digitsRemaining--;
+                }
+            }
+            System.out.println(increment);
+            out+=increment;
+        }
+        return out;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
