d56a8e677ab7b91539424790685102eadc30857b
JayShamis <jays63@nycstudents.net>
2025-12-10 10:18:19 -0500

    Start setup for day 4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-//        ArrayList<String> lines = getFileData("src/data");
-//        String[] ranges = getFileData("src/data").get(0).split(",");
         ArrayList<String> data=getFileData("src/data");
+        String[][] DAY4=new String[data.get(0).length()+2][data.size()+2];
         int partOneAnswer;
-        long partTwoAnswer;
+        int partTwoAnswer;
         partOneAnswer=getPartOneNumber(data);
         partTwoAnswer=getPartTwoNumber(data);
         System.out.println("Part one answer: " + partOneAnswer);
@@ -20,169 +18,11 @@ public class Main {
 
     public static int getPartOneNumber(ArrayList<String> input) {
         int out=0;
-        String newRes="";
-        boolean caseB;
-        for (int lines = 0; lines < input.size(); lines++) {
-            int thing=0;
-            caseB=false;
-            for (int i = 9; i >0; i--) {
-                for (int j = 0; j < input.get(lines).length(); j++) {
-                    if (Integer.parseInt(input.get(lines).substring(j, j+1))==i){
-                        if (j!=input.get(lines).length()-1){
-                            newRes=input.get(lines).substring(j+1);
-                            thing+=i*10;
-                            i=0;
-                        } else {newRes=input.get(lines).substring(0, input.get(lines).length()-1); thing+=i; i=0; caseB=true;}
-                    }
-                }
-            }
-            for (int i = 9; i >0; i--) {
-                for (int j = 0; j < newRes.length(); j++) {
-                    if (Integer.parseInt(newRes.substring(j, j+1))==i){
-                        if (caseB){
-                            thing+=10*i;
-                        } else {
-                            thing += i;
-                        }
-                        i=0;
-                    }
-                }
-            }
-            System.out.println(thing);
-            out+=thing;
-        }
         return out;
     }
 
-    public static long getPartTwoNumber(ArrayList<String> input) {
-        long out=0L;
-        for (int i = 0; i < input.size(); i++) {//Loops through each input
-            long number=0L;
-            String res=input.get(i);
-            int x=11;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the first digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the second digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the third digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the fourth digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the fifth digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the sixth digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the seventh digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the eighth digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the ninth digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the tenth digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the eleventh digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            x--;
-            for (int j = 9; j > 0; j--) {//GOAL: Capture the twelfth digit
-                for (int k = 0; k <res.length()-x; k++) {
-                    if (Integer.parseInt(res.substring(k, k+1))==j){
-                        number+= ((long)j*(long)Math.pow(10, x));
-                        res=res.substring(k+1);
-                        j=0;
-                    }
-                }
-            }
-            System.out.println(input.get(i));
-            System.out.println(number);
-            out+=number;
-        }
+    public static int getPartTwoNumber(ArrayList<String> input) {
+        int out=0;
         return out;
     }
 
