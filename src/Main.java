c8e1a661b230f704e4d06a495682c477e1df43f1
JayShamis <jays63@nycstudents.net>
2025-12-11 10:18:15 -0500

    I loathe my inefficiency

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,102 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> data=getFileData("src/data");
-        String[][] DAY4=new String[data.get(0).length()+2][data.size()+2];
+        String[][] DAY4=new String[data.size()+2][data.getFirst().length()+2];
+        for (int i = 1; i < data.size()+1; i++) {
+            for (int j = 1; j < DAY4[i].length-1; j++) {
+                DAY4[i][j]= data.get(i-1).substring(j-1, j);
+            }
+        }
+        for (int i = 0; i < DAY4.length; i++) {
+            for (int j = 0; j < DAY4[i].length; j++) {
+                if (DAY4[i][j]==null)
+                    DAY4[i][j]=".";
+            }
+        }
         int partOneAnswer;
         int partTwoAnswer;
-        partOneAnswer=getPartOneNumber(data);
-        partTwoAnswer=getPartTwoNumber(data);
+        partOneAnswer=getPartOneNumber(DAY4);
+        partTwoAnswer=getPartTwoNumber(DAY4);
         System.out.println("Part one answer: " + partOneAnswer);
         System.out.println("Part two answer: " + partTwoAnswer);
     }
 
-    public static int getPartOneNumber(ArrayList<String> input) {
+    public static int getPartOneNumber(String[][] in) {
         int out=0;
+        for (int i = 1; i < in.length; i++) {
+            for (int j = 1; j < in[i].length; j++) {
+                if (in[i][j].equals("@")){
+                    int count=0;
+                    if (in[i+1][j].equals("@")){
+                        count++;
+                    }
+                    if (in[i-1][j].equals("@")){
+                        count++;
+                    }
+                    if (in[i][j-1].equals("@")){
+                        count++;
+                    }
+                    if (in[i][j+1].equals("@")){
+                        count++;
+                    }if (in[i+1][j+1].equals("@")){
+                        count++;
+                    }
+                    if (in[i+1][j-1].equals("@")){
+                        count++;
+                    }
+                    if (in[i-1][j-1].equals("@")){
+                        count++;
+                    }
+                    if (in[i-1][j+1].equals("@")){
+                        count++;
+                    }
+                    if (count<4)
+                        out++;
+                }
+            }
+        }
         return out;
     }
 
-    public static int getPartTwoNumber(ArrayList<String> input) {
+    public static int getPartTwoNumber(String[][] in) {
         int out=0;
+        for (int z=0; z<(in.length-2)*(in[0].length-2); z++) {
+            for (int i = 1; i < in.length; i++) {
+                for (int j = 1; j < in[i].length; j++) {
+                    if (in[i][j].equals("@")) {
+                        int count = 0;
+                        if (in[i + 1][j].equals("@")) {
+                            count++;
+                        }
+                        if (in[i - 1][j].equals("@")) {
+                            count++;
+                        }
+                        if (in[i][j - 1].equals("@")) {
+                            count++;
+                        }
+                        if (in[i][j + 1].equals("@")) {
+                            count++;
+                        }
+                        if (in[i + 1][j + 1].equals("@")) {
+                            count++;
+                        }
+                        if (in[i + 1][j - 1].equals("@")) {
+                            count++;
+                        }
+                        if (in[i - 1][j - 1].equals("@")) {
+                            count++;
+                        }
+                        if (in[i - 1][j + 1].equals("@")) {
+                            count++;
+                        }
+                        if (count < 4) {
+                            out++;
+                            in[i][j] = ".";
+                        }
+                    }
+                }
+            }
+        }
         return out;
     }
 
